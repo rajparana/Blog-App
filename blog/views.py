a6c9cfa452124deeffe1d07f135121e57ef9f542
Your Name <you@example.com>
2022-06-29 10:38:46 +0530

    Design the template

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import get_object_or_404
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    print(posts,  'ppppppppppppppppppp')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_new(request):
@@ -20,13 +22,13 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('post_detail', id=post.id)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+def post_edit(request, id):
+    post = get_object_or_404(Post, id=id)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
@@ -34,7 +36,7 @@ def post_edit(request, pk):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('post_detail', id=post.id)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
